c9ca64ee981d3d2b413d180faea4e1aad51086e5
Jeremiah Thuku <jeremiah.thuku@jubileekenya.com>
2019-09-09 13:18:34 +0300

    added the appbar for home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -34,58 +34,142 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
 	
 	@override
 	Widget build(BuildContext context) {
-		return Container(
-			margin: EdgeInsets.symmetric(horizontal: 16.0,),
-			child: Column(
-				crossAxisAlignment: CrossAxisAlignment.start,
-				mainAxisSize: MainAxisSize.max,
-				children: <Widget>[
-					SizedBox(height: 50,),
-					TabBar(
-						controller: _tabController,
-						isScrollable: true,
-						indicatorSize: TabBarIndicatorSize.label,
-						indicatorColor: Colors.transparent,
-						labelPadding: EdgeInsets.all(4.0),
-						indicatorWeight: 1.0,
-						indicatorPadding: EdgeInsets.all(0),
-						tabs: List<Widget>.generate(listCategories.length, (int index){
-							return Container(
-								margin: EdgeInsets.only(right: 8.0),
-								child: Column(
-									mainAxisAlignment: MainAxisAlignment.center,
-									children: <Widget>[
-										Text(
-											listCategories[index],
-											style: TextStyle(
-												fontWeight: FontWeight.bold,
-												fontSize: 24.0,
-												color: _activeTabIndex == index ? Colors.blue.shade400 : Colors.grey.shade400,
+		return Scaffold(
+			body: Container(
+				margin: EdgeInsets.symmetric(horizontal: 16.0,),
+				child: Column(
+					crossAxisAlignment: CrossAxisAlignment.start,
+					mainAxisSize: MainAxisSize.max,
+					children: <Widget>[
+						SizedBox(height: 40,),
+						PreferredSize(
+							child: Row(
+								mainAxisAlignment: MainAxisAlignment.spaceBetween,
+								children: <Widget>[
+									Expanded(
+										child: _CategoryDropMenu(),
+									),
+									Icon(
+										Icons.shopping_basket,
+										size: 30,
+										color: Colors.black,
+									),
+									SizedBox(width: 10.0,),
+									Icon(
+										Icons.search,
+										size: 30,
+										color: Colors.black,
+									),
+								],
+							),
+							preferredSize: Size(
+								MediaQuery.of(context).size.width,
+								60,
+							),
+						),
+						SizedBox(height: 20,),
+						TabBar(
+							controller: _tabController,
+							isScrollable: true,
+							indicatorSize: TabBarIndicatorSize.label,
+							indicatorColor: Colors.transparent,
+							labelPadding: EdgeInsets.all(4.0),
+							indicatorWeight: 1.0,
+							indicatorPadding: EdgeInsets.all(0),
+							tabs: List<Widget>.generate(listCategories.length, (int index){
+								return Container(
+									margin: EdgeInsets.only(right: 8.0),
+									child: Column(
+										mainAxisAlignment: MainAxisAlignment.center,
+										children: <Widget>[
+											Text(
+												listCategories[index],
+												style: TextStyle(
+													fontWeight: FontWeight.bold,
+													fontSize: 22.0,
+													color: _activeTabIndex == index ? Colors.blue.shade400 : Colors.grey.shade400,
+												),
 											),
-										),
-										SizedBox(height: 8.0,),
-										Container(
-											width: 8.0,
-											height: 8.0,
-											decoration: BoxDecoration(
-												color: _activeTabIndex == index ? Colors.blueAccent : Colors.transparent,
-												borderRadius: BorderRadius.circular(30.0),
+											SizedBox(height: 8.0,),
+											Container(
+												width: 8.0,
+												height: 8.0,
+												decoration: BoxDecoration(
+													color: _activeTabIndex == index ? Colors.blueAccent : Colors.transparent,
+													borderRadius: BorderRadius.circular(30.0),
+												),
 											),
-										),
-									],
-								)
-							);
-						}),
-					),
-				],
+										],
+									)
+								);
+							}),
+						),
+					],
+				),
 			),
 		);
 	}
 	
 	@override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    _tabController.dispose();
-  }
+	void dispose() {
+		super.dispose();
+		_tabController.dispose();
+	}
+}
+
+class _CategoryDropMenu extends StatefulWidget {
+	@override
+	State<StatefulWidget> createState() {
+		return _CategoryDropMenuState();
+	}
+}
+
+class _CategoryDropMenuState extends State<_CategoryDropMenu>{
+	
+	static List<String> _categories = ["Popular","Trending","New Arrivals"];
+	
+	String _dropdownValue = _categories[0].toString();
+	
+	@override
+	Widget build(BuildContext context) {
+		
+		return PopupMenuButton(
+			onSelected: (index){
+				setState(() {
+					_dropdownValue = index;
+				});
+			},
+			child: Row(
+				mainAxisSize: MainAxisSize.min,
+				children: <Widget>[
+					Text(
+						_dropdownValue,
+						style: TextStyle(
+							fontSize: 24.0,
+							fontWeight: FontWeight.bold,
+						),
+					),
+					Icon(
+						Icons.keyboard_arrow_down,
+						color: Colors.black,
+					),
+				],
+			),
+			itemBuilder: (context) {
+				return _categories.map<PopupMenuItem<String>>((String value) {
+					return PopupMenuItem<String>(
+						value: value,
+						child: Text(
+							value,
+							style: TextStyle(
+								color: Colors.black,
+								fontWeight: FontWeight.bold,
+								fontSize: 24
+							),
+						),
+					);
+				}).toList();
+			}
+		);
+	}
 }
