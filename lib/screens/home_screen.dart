d43cc904a6cbba48b0c342869503218d1ccb487e
Jeremiah Thuku <jeremiah.thuku@jubileekenya.com>
2019-09-10 10:46:12 +0300

    started with grid layout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,16 +11,21 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
 	
 	List<String> listCategories = ["Bags", "Shoes", "Jackets"];
-	TabController _tabController;
 	
-	int _activeTabIndex = 1;
+	TabController _tabController;
+	int _activeTabIndex = 0;
+	
+	List<String> listProducts = ["Bags", "Shoes", "Jackets","Bags", "Shoes", "Jackets","Bags", "Shoes", "Jackets"];
+	double childAspectRatio;
+	
+	double height;
 	
 	@override
 	void initState() {
 		super.initState();
 		_tabController = TabController(
 			vsync: this,
-			length: 6,
+			length: listCategories.length,
 		);
 		
 		// within your initState() method
@@ -35,6 +40,11 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
 	
 	@override
 	Widget build(BuildContext context) {
+		
+		height = MediaQuery.of(context).size.height;
+		
+		childAspectRatio = MediaQuery.of(context).size.width / (MediaQuery.of(context).size.height / 1.4);
+		
 		return Scaffold(
 			body: Container(
 				margin: EdgeInsets.symmetric(horizontal: 16.0,),
@@ -94,22 +104,131 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
 							indicatorSize: TabBarIndicatorSize.label,
 							isScrollable: true,
 							indicatorColor: Colors.transparent,
-							labelPadding: EdgeInsets.all(4.0),
+							labelPadding: EdgeInsets.all(0.0),
 							indicatorWeight: 1.0,
 							indicatorPadding: EdgeInsets.all(0),
 							tabs: createTabs(),
 						),
+						SizedBox(height: 4,),
+						Expanded(
+							child: TabBarView(
+								controller: _tabController,
+								children: createTabBarViews(),
+							),
+						),
+						SizedBox(height: 10,),
 					],
 				),
 			),
 		);
 	}
 	
+	List<Widget> createTabBarViews(){
+		return List<Widget>.generate(listCategories.length, (int index) {
+			return index == 0 ?
+			GridView.builder(
+				gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+					crossAxisCount: 2,
+					childAspectRatio: childAspectRatio,
+				),
+				itemCount: listProducts.length,
+				itemBuilder: (BuildContext context, int index){
+					return Padding(
+						padding: EdgeInsets.all(2.0),
+						child: Material(
+							borderRadius: BorderRadius.all(
+								Radius.circular(10.0),
+							),
+							
+							child: ClipRRect(
+								borderRadius: BorderRadius.all(Radius.circular(10.0)),
+								child: Material( // needed
+									color: Colors.white,
+									child: InkWell(
+										splashColor: Colors.blue.shade600,
+										onTap: (){
+										
+										},
+										child: Container(
+											padding: EdgeInsets.fromLTRB(16, 16, 16, 8),
+											decoration: BoxDecoration(
+												borderRadius: BorderRadius.all(
+													Radius.circular(10.0),
+												),
+												border: Border.all(
+													color: Colors.grey.shade300,
+													width: 0.5,
+												),
+											),
+											child: Column(
+												mainAxisSize: MainAxisSize.max,
+												crossAxisAlignment: CrossAxisAlignment.center,
+												children: <Widget>[
+													Center(
+														child: Hero(tag: "bagType",
+															child: Image.asset(
+																"assets/images/red_bag.png",
+																fit: BoxFit.cover,
+																height: height / 5,
+															)
+														),
+													),
+													SizedBox(height: 4.0,),
+													Expanded(
+														child: Column(
+															mainAxisSize: MainAxisSize.max,
+															mainAxisAlignment: MainAxisAlignment.center,
+															children: <Widget>[
+																Text(
+																	"Blue Pouch",
+																	style: TextStyle(
+																		fontWeight: FontWeight.bold,
+																		fontSize: 20,
+																		color: Colors.black,
+																	),
+																	textAlign: TextAlign.center,
+																),
+																SizedBox(height: 4.0,),
+																Text(
+																	"\$99",
+																	style: TextStyle(
+																		fontWeight: FontWeight.bold,
+																		fontSize: 16,
+																		color: Colors.blueAccent,
+																	),
+																	textAlign: TextAlign.center,
+																),
+															],
+														),
+													),
+												],
+											),
+										),
+									)
+								),
+							),
+						),
+					);
+				},
+			)
+				:
+			Center(
+				child:Text(
+					listCategories[index].toString(),
+					style: TextStyle(
+						fontSize: 24,
+						fontWeight: FontWeight.bold,
+					),
+				),
+			);
+		});
+	}
+	
 	// the tabs to show
 	List<Widget> createTabs() {
 		return List<Widget>.generate(listCategories.length, (int index){
 			return Container(
-				padding: EdgeInsets.only(right: 30.0),
+				padding: EdgeInsets.only(right: 24.0),
 				child: Column(
 					mainAxisSize: MainAxisSize.min,
 					crossAxisAlignment: CrossAxisAlignment.center,
